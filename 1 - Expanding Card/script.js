ae8d880e3961d3c9ec3dc6a71d423c21962c0dc4
Hamza-Nawabi <111171188+Nawabi-Hamza@users.noreply.github.com>
2025-04-13 15:29:26 +0430

    Expanded card has finished

--- 1 - Expanding Card/script.js
+++ 1 - Expanding Card/script.js
@@ -1,1 +1,9 @@
 console.log("Script loaded");
+
+
+const panels = document.querySelectorAll(".panel");
+
+panels.forEach( panel => panel.addEventListener("click", (e) => {
+    panels.forEach( panel => panel.classList.remove("active"))
+    panel.classList.add("active")
+}))
