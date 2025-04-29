a4c55a22c2988bf374c2ef04dd65ee661ba0a258
Hamza-Nawabi <hamza.nawabi119@gmail.com>
2025-04-29 09:52:50 +0430

    Rotating Navigation finished

--- 3 - Rotating Navigation/script.js
+++ 3 - Rotating Navigation/script.js
@@ -1,1 +1,16 @@
 console.log("Script loaded");
+
+const container = document.querySelector(".container")
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+const circle = document.querySelector(".circle")
+
+open.addEventListener("click",()=>{
+    container.classList.add("show-nav");
+    circle.style = "transform: rotate(-90deg)"
+})
+close.addEventListener("click",()=>{
+    container.classList.remove("show-nav");
+    circle.style = "transform: rotate(0deg)"
+
+})
