f21e189f3a428cb9dbd505254a8aacd2f5ae02fb
Hamza-Nawabi <hamza.nawabi119@gmail.com>
2025-05-30 05:23:58 +0430

    Jokes

--- 10 - Dad Joke/script.js
+++ 10 - Dad Joke/script.js
@@ -1,1 +1,22 @@
 
+const jokeElement = document.querySelector(".joke h3");
+const jokeBtn = document.getElementById("jokeBtn")
+
+async function fetchJoke(){
+    fetch("https://icanhazdadjoke.com",{
+        headers: {
+            "Accept": "application/json"
+        }
+    })
+    .then( res => res.json())
+    .then( data => {
+        if(data?.status==200){
+            return jokeElement.innerText = data.joke
+        }
+        return jokeElement.innerText = "Error fetching joke !"
+    })
+}
+fetchJoke()
+
+
+jokeBtn.addEventListener("click", fetchJoke)
