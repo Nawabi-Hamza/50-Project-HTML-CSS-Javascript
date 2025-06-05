b27ba6f25a05ff0015730ca77864cd1d2f9604fd
Hamza-Nawabi <hamza.nawabi119@gmail.com>
2025-06-05 13:57:14 +0430

    incrementing counter project done

--- 15 - Incrementing Counter/script.js
+++ 15 - Incrementing Counter/script.js
@@ -1,3 +1,28 @@
 
 
-console.log('script loaded')
+
+document.addEventListener("DOMContentLoaded",function(){
+    const counters = document.querySelectorAll(".counter")
+    counters.forEach( counter => {
+        counter.innerText = '0';
+
+        const updateCounter = ()=>{
+            const target  = +counter.getAttribute("data-target")
+            const c = +counter.innerText
+            const increment = target / 200
+            if(c < target){
+                counter.innerText = `${Math.ceil(c+increment)}`
+                setTimeout(updateCounter, 1)
+            }else counter.innerText = target
+            // console.log(increment)
+        }
+
+        updateCounter()
+        // console.log(counter.getAttribute("data-target"))
+        // for(let i=0; i<= parseInt(counter.getAttribute("data-target")) ; i++){
+        //     // console.log(i)
+        //     counter.textContent = i;
+        // }
+
+    })
+})
